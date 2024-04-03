4e74ee821d0096511c720a6a3fdd784f18b67a3d
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2024-04-03 03:23:31 -0300

    Configura banco de dados postgres e classe de autenticação

--- backend/project_authentication/settings.py
+++ backend/project_authentication/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = "project_authentication.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.environ["DB_ENGINE"],
+        "NAME": os.environ["DB_NAME"],
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
+        "HOST": os.environ["DB_HOST"],
+        "PORT": os.environ["DB_PORT"],
     }
 }
 
@@ -133,4 +137,6 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {"SIGNING_KEY": os.environ["SIGNING_KEY"]}
+
+AUTH_USER_MODEL = "authentication.User"
 
