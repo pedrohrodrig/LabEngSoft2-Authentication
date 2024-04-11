23b51e1e5d799ad34b61908a77e55ba306d9e0f3
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2024-04-11 02:48:42 -0300

    Adiciona / ao final

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -7,8 +7,8 @@ from rest_framework_simplejwt.views import (
 from .views import UserView
 
 urlpatterns = [
-    path("token", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("register/", UserView.as_view(actions={"post": "register"})),
     path("user/", UserView.as_view(actions={"get": "list"})),
     path("user/self/", UserView.as_view(actions={"get": "retrieve_self"})),
