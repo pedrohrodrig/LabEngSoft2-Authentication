5d69c76cbfc90ccea825119b2cc3b117865f73d3
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2024-04-15 02:44:54 -0300

    Altera nome do db

--- backend/project_authentication/settings.py
+++ backend/project_authentication/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = "project_authentication.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME", "authentication_service_db"),
+        "NAME": os.getenv("DB_NAME", "postgres"),
         "USER": os.getenv("DB_USER", "postgres"),
         "PASSWORD": os.getenv("DB_PASSWORD", "postgres"),
         "HOST": os.getenv("DB_HOST", "authentication-service-db.cpssys6guvh3.sa-east-1.rds.amazonaws.com"),
